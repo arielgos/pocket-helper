0af01b8525e5bccfc86f8af6e496e6a67a2bee42
Ariel Ortuño Solis <arielg.os@gmail.com>
2026-07-31 11:02:39 -0400

    cloud function fixes

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,10 +1,16 @@
-import { setGlobalOptions } from "firebase-functions";
-import { onRequest } from "firebase-functions/https";
+import { setGlobalOptions } from "firebase-functions/v2";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
+// Configure global options for v2 functions
 setGlobalOptions({ maxInstances: 10 });
 
 export const health = onRequest((request, response) => {
-  logger.info("Health check", { structuredData: true }, request, response);
+  // Log request details safely (do NOT pass raw request or response objects)
+  logger.info("Health check received", {
+    method: request.method,
+    path: request.path,
+  });
+
   response.send("OK");
 });
